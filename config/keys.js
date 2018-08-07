4e3107623ccf720bf6ab5cfc17f48d1c01dc288c
Miguel Enrique Guerra Topete <guetop@MacBook-Pro-de-Miguel.local>
2018-08-07 11:46:05 -0500

    finished auth flow2

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // keys.js - figure out what set of credentials to return
-if (proccess.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
   module.exports = require('./prod');
 } else {
