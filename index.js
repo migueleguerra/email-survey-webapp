9a23c600b11dd0e37ac645b06589f019e381e2e9
Miguel Enrique Guerra Topete <guetop@MacBook-Pro-de-Miguel.local>
2018-09-12 13:37:25 -0500

    survey routes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets. Like our main.js, or main.cs file!
